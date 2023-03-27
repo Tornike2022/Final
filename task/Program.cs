c8562da81058a980dbbd8db59f8d21a4b6d170a2
Teona Gogichashvili <teonafreckling@gmail.com>
2023-03-27 15:39:53 +0300

    added the counter

---
+++ task/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] array1 = new string[5] {"-2", "23", "hello", "Kazan", "res"};
+string[] array2 = new string[array1.Length];
+void ThreeDigitOrLessArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
